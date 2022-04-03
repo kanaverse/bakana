2c49777de0298d7c50fcb40ca39f9514b2e6e18b
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-04-03 01:43:54 -0700

    Added downloader scripts to prep for tests.

---
+++ files/datasets/download.sh
@@ -0,0 +1,7 @@
+base=https://github.com/jkanche/random-test-files/releases/download
+
+# Download PBMC datasets.
+curl -L ${base}/10x-pbmc-v1.0.0/pbmc3k-matrix.mtx.gz > pbmc3k-matrix.mtx.gz
+curl -L ${base}/10x-pbmc-v1.0.0/pbmc3k-features.tsv.gz > pbmc3k-features.tsv.gz
+curl -L ${base}/10x-pbmc-v1.0.0/pbmc3k-barcodes.tsv.gz > pbmc3k-barcodes.tsv.gz
+curl -L ${base}/10x-pbmc-v1.0.0/pbmc4k-tenx.h5 > pbmc4k-tenx.h5
---
+++ files/references/download.sh
@@ -0,0 +1,7 @@
+base=https://github.com/clusterfork/singlepp-references/releases/download
+
+curl -L ${base}/hs-latest/BlueprintEncode_genes.csv.gz > BlueprintEncode_genes.csv.gz
+curl -L ${base}/hs-latest/BlueprintEncode_matrix.csv.gz > BlueprintEncode_matrix.csv.gz
+curl -L ${base}/hs-latest/BlueprintEncode_label_names_fine.csv.gz > BlueprintEncode_label_names_fine.csv.gz
+curl -L ${base}/hs-latest/BlueprintEncode_labels_fine.csv.gz > BlueprintEncode_labels_fine.csv.gz
+curl -L ${base}/hs-latest/BlueprintEncode_markers_fine.gmt.gz > BlueprintEncode_markers_fine.gmt.gz
