048295a02c5ee1b7707e9366792ad42305b2d627
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-04-04 11:05:40 -0700

    Added builder script to organize files for Node vs browser.

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+set -u
+
+mode=$(echo $1 | sed "s/\/$//")
+if [ $mode != "module" ] && [ $mode != "main" ]
+then
+    echo "need to specify 'module' or 'main' as the first argument"
+    exit 1
+fi
+
+if [ $mode == "main" ]
+then
+    toss=web
+    keep=node
+else
+    toss=node
+    keep=web
+fi
+
+rm -rf ${mode}
+mkdir -p ${mode}
+cp -r src/* ${mode}
+rm ${mode}/abstract/*_${toss}.js
+
+to_rename=$(ls ${mode}/abstract/*_${keep}.js)
+for x in ${to_rename[@]}
+do
+    newname=$(echo $x | sed "s/_${keep}\\.js$/.js/g")
+    mv $x $newname
+done
