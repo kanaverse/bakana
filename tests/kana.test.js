e1c549ee82dfad3e604a515c28a52ac1d7db388b
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-04-07 22:09:43 -0700

    Added tests for creation of linked kana files.

--- tests/kana.test.js
+++ tests/kana.test.js
@@ -6,16 +6,16 @@ import * as fs from "fs";
 beforeAll(async () => await bakana.initialize({ localFile: true }));
 afterAll(async () => await bakana.terminate());
 
-test("saving to and loading from a kana file works correctly", async () => {
-    let files = { 
-        default: {
-            format: "MatrixMarket",
-            mtx: "files/datasets/pbmc3k-matrix.mtx.gz",
-            genes: "files/datasets/pbmc3k-features.tsv.gz",
-            annotations: "files/datasets/pbmc3k-barcodes.tsv.gz"
-        }
+let files = { 
+    default: {
+        format: "MatrixMarket",
+        mtx: "files/datasets/pbmc3k-matrix.mtx.gz",
+        genes: "files/datasets/pbmc3k-features.tsv.gz",
+        annotations: "files/datasets/pbmc3k-barcodes.tsv.gz"
     }
+}
 
+test("saving to and loading from a kana file works correctly (embedded)", async () => {
     let params = utils.baseParams();
     let state = await bakana.createAnalysis();
     await bakana.runAnalysis(state, files, params);
@@ -37,11 +37,43 @@ test("saving to and loading from a kana file works correctly", async () => {
         fs.rmSync(tmp, { recursive: true, force: true });
     }
     fs.mkdirSync(tmp);
-    const round_trip = "TEST_kana_state2.h5";
+    const round_trip = "TEST_kana_state_again.h5";
 
     let loader = await bakana.parseKanaFile(kpath, round_trip, { stageDir: tmp });
     let reloaded = await bakana.loadAnalysis(round_trip, loader);
 
     // Checking that we got something that was reasonable.
     expect(reloaded.state.pca.summary()).toEqual(ref_pca);
+})
+
+test("saving to and loading from a kana file works with links", async () => {
+    let params = utils.baseParams();
+    let state = await bakana.createAnalysis();
+    await bakana.runAnalysis(state, files, params);
+    let ref_pca = state.pca.summary();
+
+    // Links just re-use the file path for our Node tests, which is unique enough!
+    let old_create = bakana.setCreateLink(x => x);
+    let old_resolve = bakana.setResolveLink(x => x)
+
+    // Saving to a kana file.
+    const path = "TEST_kana_state2.h5";
+    await bakana.saveAnalysis(state, path, { embedded: false });
+    bakana.freeAnalysis(state);
+
+    const kpath = "TEST_state2.kana";
+    let res = await bakana.createKanaFile(path, null, { outputPath: kpath });
+    expect(fs.statSync(res).size).toBe(24 + fs.statSync(path).size);
+
+    // Alright - trying to unpack everything.
+    const round_trip = "TEST_kana_state_again2.h5";
+    let loader = await bakana.parseKanaFile(kpath, round_trip);
+    let reloaded = await bakana.loadAnalysis(round_trip, loader);
+
+    // Checking that we got something that was reasonable.
+    expect(reloaded.state.pca.summary()).toEqual(ref_pca);
+
+    // Reverting the linkers.
+    bakana.setCreateLink(old_create);
+    bakana.setResolveLink(old_resolve);
 })
