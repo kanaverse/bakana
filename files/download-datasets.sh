405d742e726e53e0e7b9068f37bb6e71af1689ce
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-09-26 22:23:41 -0700

    Download RDS files for testing.

--- files/download-datasets.sh
+++ files/download-datasets.sh
@@ -27,6 +27,7 @@ download 10x-pbmc-v1.0.0/combined-barcodes.tsv.gz pbmc-combined-barcodes.tsv.gz
 
 # Download Zeisel datasets.
 download zeisel-brain-v1.0.0/csc.h5ad zeisel-brain.h5ad
+download zeisel-brain-v1.0.0/csc.rds zeisel-brain.rds
 
 # Download immune datasets.
 download 10x-immune-v1.0.0/immune_3.0.0_sub-matrix.mtx.gz immune_3.0.0-matrix.mtx.gz
