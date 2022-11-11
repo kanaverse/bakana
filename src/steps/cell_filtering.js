d51de171a0b2b5168dfd35b71fcdc13d6b551a17
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-11-11 13:09:11 -0800

    Renamed and documented the property.

--- src/steps/cell_filtering.js
+++ src/steps/cell_filtering.js
@@ -250,17 +250,19 @@ export class CellFilteringState {
      * @return {Object} An object containing:
      *
      * - `retained`: the number of cells retained after filtering out low-quality cells.
+     * - `discard`: a Uint8Array specifying the cells to be discarded.
+     *   Alternatively, this may be `null` if no quality control was requested.
      */
     summary() {
-        let remaining = 0, subset_vec = null;
+        let remaining = 0, discard_vec = null;
         if ("discard_buffer" in this.#cache) {
             this.#cache.discard_buffer.forEach(x => { remaining += (x == 0); });
-            subset_vec = this.#cache.discard_buffer.slice();
+            discard_vec = this.#cache.discard_buffer.slice();
         } else {
             let available = this.#inputs.hasAvailable();
             remaining = this.#inputs.fetchCountMatrix(available[0]).numberOfColumns();
         }
-        return { "retained": remaining, "subset": subset_vec};
+        return { "retained": remaining, "discard": discard_vec};
     }
 
     /*************************
