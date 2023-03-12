d8c8fc3fdcaff80aa8f66a6095845a586cee319b
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-03-12 11:56:04 -0700

    Fix MD5 sum calculations on the browser.

--- src/dump/abstract/dump_web.js
+++ src/dump/abstract/dump_web.js
@@ -3,6 +3,9 @@ import * as scran from "scran.js";
 
 export async function attachMd5sums(files) {
     for (const x of files) {
+        if (!("contents" in x)) {
+            continue;
+        }
         x.metadata.md5sum = await md5(x.contents);
     }
 }
