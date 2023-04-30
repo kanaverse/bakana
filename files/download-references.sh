ed91965ff5abc4b8976349d9fbbec843c687fa6a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-04-30 00:05:49 -0700

    Added back the ImmGen downloads.

--- files/download-references.sh
+++ files/download-references.sh
@@ -22,12 +22,14 @@ download BlueprintEncode_labels_fine.csv.gz BlueprintEncode_labels_fine.csv.gz
 download BlueprintEncode_markers_fine.gmt.gz BlueprintEncode_markers_fine.gmt.gz
 
 download ImmGen_genes_symbol.csv.gz ImmGen_genes_symbol.csv.gz
+download ImmGen_genes_ensembl.csv.gz ImmGen_genes_ensembl.csv.gz
 download ImmGen_matrix.csv.gz ImmGen_matrix.csv.gz
 download ImmGen_label_names_fine.csv.gz ImmGen_label_names_fine.csv.gz
 download ImmGen_labels_fine.csv.gz ImmGen_labels_fine.csv.gz
 download ImmGen_markers_fine.gmt.gz ImmGen_markers_fine.gmt.gz
 
 download MouseRNAseq_genes_symbol.csv.gz MouseRNAseq_genes_symbol.csv.gz
+download MouseRNAseq_genes_ensembl.csv.gz MouseRNAseq_genes_ensembl.csv.gz
 download MouseRNAseq_matrix.csv.gz MouseRNAseq_matrix.csv.gz
 download MouseRNAseq_label_names_fine.csv.gz MouseRNAseq_label_names_fine.csv.gz
 download MouseRNAseq_labels_fine.csv.gz MouseRNAseq_labels_fine.csv.gz
